5e2f5105849c5150f95d37e22608f6ed37294da5
serephus <i@sereph.us>
2026-05-02 21:02:08 +0800

    template: add cpp template

---
+++ modules/templates/_cpp/flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              cmake
+              clang-tools
+            ];
+          };
+      }
+    );
+}
