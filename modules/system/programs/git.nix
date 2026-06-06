19c8ac32d7dacb8759bc21f6b01d94af15bd64e8
serephus <i@sereph.us>
2026-06-06 21:56:06 +0800

    git: add alias la

--- modules/system/programs/git.nix
+++ modules/system/programs/git.nix
@@ -32,6 +32,7 @@
             alias = {
               count-lines = "! git log --author=\"$1\" --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf \"added lines: %s, removed lines: %s, total lines: %s\\n\", add, subs, loc }' #";
               lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+              la = "log --color --graph --all --branches";
             };
             init.defaultBranch = "main";
           };
