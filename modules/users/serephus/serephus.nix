e0a19023ab68dbf60fcd6d29c447dc18b79a92b3
serephus <i@sereph.us>
2026-06-06 21:54:15 +0800

    serephus: add ssh keys

--- modules/users/serephus/serephus.nix
+++ modules/users/serephus/serephus.nix
@@ -9,6 +9,9 @@ in
       nixos."${username}" = _: {
         users.users."${username}" = {
           hashedPassword = "$y$j9T$Kd.XCW/gJoS41OmyJioSe1$MAk45.m/HFOYdo4WcIPlDz4X9ipmzgv3aWbRqIEaJ89";
+          openssh.authorizedKeys.keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJCl8X5dDv+19y323NZkCbSMiou8phYjbTvTUou5Ju+w i@sereph.us"
+          ];
         };
       };
 
