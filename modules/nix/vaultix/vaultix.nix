50ab04d4ba70b61c0f66e0b46338918d65c29bfc
serephus <i@sereph.us>
2026-07-02 22:40:34 +0800

    vaultix: replace key file with yubikey

--- modules/nix/vaultix/vaultix.nix
+++ modules/nix/vaultix/vaultix.nix
@@ -3,6 +3,7 @@
   flake.vaultix = {
     # vaultix configs
     nodes = self.outputs.nixosConfigurations;
-    identity = "./secrets/key.txt";
+    # identity = "./secrets/key.txt";
+    identity = "./secrets/age-yubikey-identity-af10df80.txt";
   };
 }
