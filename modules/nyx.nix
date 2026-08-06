7ce934f8b9c212185a65350feef7f92e1d64813f
dearfl <i@flr.me>
2026-08-06 20:23:12 +0800

    flake: refactor

--- modules/nyx.nix
+++ modules/nyx.nix
@@ -7,7 +7,6 @@
       (den.aspects.vaultix "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKXvckmMZo48If0O1qTTnQRjMeiARAp7sfWNDbX8p6Eu")
       den.aspects.preservation
       den.aspects.wifi
-      den.aspects.ssh
     ];
 
     # host NixOS configuration
