ba19e98fe45d1fa66d0aa18a8260ec0266a9544d
dearfl <i@flr.me>
2026-08-06 12:28:17 +0800

    wifi: update password

--- modules/wifi.nix
+++ modules/wifi.nix
@@ -5,7 +5,7 @@
         enable = true;
         # hashed password should be safe?
         networks."glwifi-5g" = {
-          pskRaw = "513ce170f82b9f9bba9a5a9ddc5db9a62a26903dc9eaed7e9059502c6169440a";
+          pskRaw = "622d17f9b063286b5324a7e777da9b6f6a46d3aeb7fc64053aae89c98a7cc810";
           priority = 1000;
         };
       };
