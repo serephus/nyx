65395324318b78c35e28ca7f2fbde027b33aacde
serephus <i@sereph.us>
2026-08-07 22:34:39 +0800

    remove obselete env vars

--- modules/defaults.nix
+++ modules/defaults.nix
@@ -7,10 +7,6 @@
   den.default.nixos.users.users.root.hashedPassword =
     "$y$j9T$vmrHUvAuXfmw23ZWlTAFy0$pl4hBYuUzxYPoo7Z3IAPloJ.AZN0jZDeIKgmY/qspf0";
 
-  # we always use en_US.UTF-8 locale
+  # always use en_US.UTF-8 locale
   den.default.nixos.i18n.defaultLocale = "en_US.UTF-8";
-
-  # default environment variables
-  den.default.nixos.environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  den.default.homeManager.home.sessionVariables.NIXOS_OZONE_WL = "1";
 }
