f417b928a8d5339981909b0ca94c1a427867d93d
serephus <i@sereph.us>
2026-08-08 19:31:45 +0800

    lanzaboote: enable key auto gen and auto enroll

--- modules/lanzaboote.nix
+++ modules/lanzaboote.nix
@@ -8,30 +8,27 @@
   den.aspects.lanzaboote = secureboot: {
     nixos = { lib, pkgs, ... }: {
       imports = [ inputs.lanzaboote.nixosModules.lanzaboote ];
+
       environment.systemPackages = [
         # For debugging and troubleshooting Secure Boot.
         pkgs.sbctl
       ];
 
       # Lanzaboote currently replaces the systemd-boot module.
-      # This setting is usually set to true in configuration.nix
-      # generated at installation time. So we force it to false
-      # for now.
       boot.loader.systemd-boot.enable = lib.mkForce (!secureboot);
 
       boot.lanzaboote = {
         enable = secureboot;
         pkiBundle = "/var/lib/sbctl";
+        autoGenerateKeys.enable = true;
+        autoEnrollKeys = {
+          autoReboot = true;
+          enable = true;
+        };
       };
 
       preservation.preserveAt."/persist/var" = {
-        # core files to persist
-        directories = [
-          {
-            directory = "/var/lib/sbctl";
-            inInitrd = true;
-          }
-        ];
+        directories = [ "/var/lib/sbctl" ];
       };
     };
   };
