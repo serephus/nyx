48202d9a7f67d6b7d90868430f85cf0facfb25ab
serephus <i@sereph.us>
2026-08-08 10:29:20 +0800

    liveiso: add

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -5,6 +5,9 @@
   # serephus user at x1c host.
   den.hosts.x86_64-linux.x1c.users.serephus = { };
 
+  # serephus user at nyx host.
+  den.hosts.x86_64-linux.liveiso.users.serephus = { };
+
   # define an standalone home-manager for serephus
   # how do I add configs to standalone home configs
   den.homes.x86_64-linux."serephus@nyx" = { };
---
+++ modules/liveiso.nix
@@ -0,0 +1,43 @@
+{ den, inputs, ... }: {
+  # minimal installation live iso
+  den.aspects.liveiso = {
+    includes = [
+      den.batteries.hostname
+      (den.aspects.preservation false)
+
+      den.aspects.ssh
+      den.aspects.doc
+
+      den.aspects.git
+      den.aspects.fish
+      den.aspects.helix
+      den.aspects.tmux
+
+      den.aspects.wifi
+    ];
+
+    # host NixOS configuration
+    nixos = { lib, modulesPath, ... }: {
+      imports = [
+        (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
+      ];
+
+      time.timeZone = "Asia/Shanghai";
+      boot.zfs.forceImportRoot = lib.mkForce false;
+      networking.networkmanager.enable = lib.mkForce false;
+    };
+  };
+
+  den.schema.flake-system.includes = [ den.aspects.buildiso ];
+
+  den.aspects.buildiso = {
+    packages =
+      { ... }:
+      let
+        host = inputs.self.nixosConfigurations.liveiso.config;
+      in
+      {
+        buildiso = host.system.build.isoImage;
+      };
+  };
+}
