68a43ef2e14ecda7bcd50553ee729efbdd1e4bc5
serephus <i@sereph.us>
2026-08-08 11:58:04 +0800

    serephus: add git user config

--- modules/serephus.nix
+++ modules/serephus.nix
@@ -11,16 +11,21 @@
       # programs.ssh.enable = true;
     };
 
-    # user can provide NixOS configurations
-    # to any host it is included on
-    provides.to-hosts.nixos = {
-      # nixos configs
-      users.users.serephus = {
-        hashedPassword = "$y$j9T$9YAnjIHNRAokTtsXAE3HD/$ZbFS7C9ATg9ZX/mxcPrJaFf1/OJnKcT7nHd3rQ2Xhv2";
-        # TODO: change to yubikey
-        openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJCl8X5dDv+19y323NZkCbSMiou8phYjbTvTUou5Ju+w i@sereph.us"
-        ];
+    provides.to-hosts = { user, ... }: {
+      nixos = {
+        users.users.serephus = {
+          hashedPassword = "$y$j9T$9YAnjIHNRAokTtsXAE3HD/$ZbFS7C9ATg9ZX/mxcPrJaFf1/OJnKcT7nHd3rQ2Xhv2";
+          # TODO: change this to yubikey
+          openssh.authorizedKeys.keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJCl8X5dDv+19y323NZkCbSMiou8phYjbTvTUou5Ju+w i@sereph.us"
+          ];
+        };
+      };
+      homeManager = {
+        programs.git.settings.user = {
+          name = "${user.userName}";
+          email = "i@sereph.us";
+        };
       };
     };
   };
