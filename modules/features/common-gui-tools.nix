2a655434843db7f7d3e03246dd45ed1014d12baf
serephus <i@sereph.us>
2026-08-13 12:26:39 +0800

    chromium: preserve chromium configs

--- modules/features/common-gui-tools.nix
+++ modules/features/common-gui-tools.nix
@@ -1,14 +1,23 @@
 {
   den.aspects.common-gui-tools = {
-    provides.to-users.homeManager = { pkgs, ... }: {
-      programs.zathura.enable = true;
-      programs.swayimg.enable = true;
-      programs.mpv.enable = true;
-      # TODO: preserve chromium config
-      programs.chromium.enable = true;
-      home.packages = [
-        pkgs.gnuplot
-      ];
+    provides.to-users = { user, ... }: {
+      nixos = {
+        # TODO: this does not preserve chromium state, just configs
+        preservation.preserveAt."/persist" = {
+          users."${user.userName}" = {
+            directories = [ ".config/chromium" ];
+          };
+        };
+      };
+      homeManager = { pkgs, ... }: {
+        programs.zathura.enable = true;
+        programs.swayimg.enable = true;
+        programs.mpv.enable = true;
+        programs.chromium.enable = true;
+        home.packages = [
+          pkgs.gnuplot
+        ];
+      };
     };
   };
 }
