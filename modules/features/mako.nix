94b9d91d92d0e2ae6829f5f1e63ef51fa0dfea5c
serephus <i@sereph.us>
2026-08-14 13:30:14 +0800

    mako: set default timeout to 30s

--- modules/features/mako.nix
+++ modules/features/mako.nix
@@ -1,7 +1,12 @@
 {
   den.aspects.mako = {
     provides.to-users.homeManager = {
-      services.mako.enable = true;
+      services.mako = {
+        enable = true;
+        settings = {
+          default-timeout = 30000;
+        };
+      };
     };
   };
 }
