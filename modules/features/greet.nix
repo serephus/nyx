2b386b7cc860bb0dc937a36a8d9c5eec7c5bf52b
serephus <i@sereph.us>
2026-08-14 13:33:14 +0800

    greet: add cage opts

--- modules/features/greet.nix
+++ modules/features/greet.nix
@@ -1,7 +1,21 @@
 {
-  den.aspects.greet.nixos = {
+  den.aspects.greet.nixos = { pkgs, ... }: {
     services.greetd.enable = true;
-    programs.regreet.enable = true;
     programs.uwsm.enable = true;
+    programs.regreet = {
+      enable = true;
+      # TODO: not working right now
+      # let not set background for now
+      cursorTheme = {
+        name = "Bibata-Modern-Ice";
+        package = pkgs.bibata-cursors;
+      };
+      cageArgs = [
+        "-s"
+        "-d"
+        "-m"
+        "last"
+      ];
+    };
   };
 }
