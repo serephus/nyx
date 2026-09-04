22aab539af0fc7f14161028290045f5a713aa869
serephus <i@sereph.us>
2026-09-04 10:57:04 +0800

    copyq: preserve copyq items

--- modules/features/copyq.nix
+++ modules/features/copyq.nix
@@ -1,9 +1,18 @@
 {
   den.aspects.copyq = {
-    provides.to-users.homeManager = {
-      services.copyq = {
-        enable = true;
-        forceXWayland = false;
+    provides.to-users = { user, ... }: {
+      nixos = {
+        preservation.preserveAt."/persist" = {
+          users."${user.userName}" = {
+            directories = [ ".local/share/copyq" ];
+          };
+        };
+      };
+      homeManager = {
+        services.copyq = {
+          enable = true;
+          forceXWayland = false;
+        };
       };
     };
   };
