f9c08900245087166389defeedc89a3ec34229bb
serephus <i@sereph.us>
2026-09-12 18:04:50 +0800

    waybar: add network module for wwan0

--- modules/features/waybar.nix
+++ modules/features/waybar.nix
@@ -9,13 +9,14 @@
 
           modules-left = lib.mkOrder 100 [ "clock" ];
 
-          modules-right = lib.mkOrder 10000 [
-            "network"
-            "cpu"
-            "memory"
-            "battery"
-            "tray"
-          ];
+          modules-right = lib.mkOrder 10000 ([ "network" ]
+            ++ lib.optionals (osConfig.xmm7360.enable or false) [ "network#wwan" ]
+            ++ [
+              "cpu"
+              "memory"
+              "battery"
+              "tray"
+            ]);
 
           clock = {
             interval = 1;
@@ -30,6 +31,17 @@
             format-disconnected = "󰯡";
             format-wifi = "󰖩 {essid}";
             interval = 1;
+          };
+
+          # Fibocom L850-GL WWAN modem (xmm7360), exposed as wwan0.
+          "network#wwan" = {
+            interface = "wwan0";
+            interval = 1;
+            format = "󰒢 {ipaddr}";
+            # hide the module entirely until wwan0 actually has an IP
+            format-disconnected = "";
+            format-linked = "";
+            tooltip-format = "{ifname} · {ipaddr}";
           };
 
           cpu = {
@@ -126,6 +138,9 @@
           }
           #network {
             border-bottom: 2px solid #8f3f71;
+          }
+          #network.wwan {
+            border-bottom: 2px solid #d79921;
           }
           #tray {
             padding: 1px 2px;
