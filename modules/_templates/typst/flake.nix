957937be34bfbf7b4a71be3fb7319a34b6a1ba81
serephus <i@sereph.us>
2026-09-19 16:48:26 +0800

    templates: add nixit

--- modules/_templates/typst/flake.nix
+++ modules/_templates/typst/flake.nix
@@ -2,12 +2,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    nixit.url = "github:serephus/nixit";
   };
 
   outputs =
     {
       nixpkgs,
       utils,
+      nixit,
       ...
     }:
     utils.lib.eachDefaultSystem (
@@ -25,6 +27,55 @@
               tinymist
             ];
           };
+
+        githubRepositories.typst = nixit.lib.githubRepository {
+          owner = "serephus";
+          name = "typst";
+
+          description = "description";
+          homepage = "homepage";
+          topics = [
+            "typst"
+          ];
+          visibility = "public";
+
+          features = {
+            wiki.enable = false;
+            issues.enable = true;
+            projects.enable = false;
+            discussions.enable = false;
+          };
+
+          is_template = false;
+          is_archived = false;
+
+          pull = {
+            merge.enable = true;
+            squash.enable = false;
+            rebase.enable = false;
+            auto_merge = true;
+            delete_branch_on_merge = true;
+            update_branch = true;
+          };
+
+          actions = {
+            enable = true;
+            policy = "all";
+            default_token_permissions = "read";
+            allow_pr_approval = false;
+          };
+
+          rulesets = {
+            default = {
+              enforcement = "active";
+              conditions.ref_name.include = [ "~DEFAULT_BRANCH" ];
+              rules = [
+                { type = "deletion"; }
+                { type = "non_fast_forward"; }
+              ];
+            };
+          };
+        };
       }
     );
 }
