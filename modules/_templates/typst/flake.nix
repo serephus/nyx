634bc4f52a2dcde79315cecd2e63bb03d189f464
serephus <i@sereph.us>
2026-09-20 10:48:13 +0800

    templates: fix nixit

--- modules/_templates/typst/flake.nix
+++ modules/_templates/typst/flake.nix
@@ -12,7 +12,57 @@
       nixit,
       ...
     }:
-    utils.lib.eachDefaultSystem (
+    {
+      githubRepositories.typst = nixit.lib.githubRepository {
+        owner = "serephus";
+        name = "typst";
+
+        description = "description";
+        homepage = "homepage";
+        topics = [
+          "typst"
+        ];
+        visibility = "public";
+
+        features = {
+          wiki.enable = false;
+          issues.enable = true;
+          projects.enable = false;
+          discussions.enable = false;
+        };
+
+        is_template = false;
+        is_archived = false;
+
+        pull = {
+          merge.enable = true;
+          squash.enable = false;
+          rebase.enable = false;
+          auto_merge = true;
+          delete_branch_on_merge = true;
+          update_branch = true;
+        };
+
+        actions = {
+          enable = true;
+          policy = "all";
+          default_token_permissions = "read";
+          allow_pr_approval = false;
+        };
+
+        rulesets = {
+          default = {
+            enforcement = "active";
+            conditions.ref_name.include = [ "~DEFAULT_BRANCH" ];
+            rules = [
+              { type = "deletion"; }
+              { type = "non_fast_forward"; }
+            ];
+          };
+        };
+      };
+    }
+    // utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
@@ -27,55 +77,6 @@
               tinymist
             ];
           };
-
-        githubRepositories.typst = nixit.lib.githubRepository {
-          owner = "serephus";
-          name = "typst";
-
-          description = "description";
-          homepage = "homepage";
-          topics = [
-            "typst"
-          ];
-          visibility = "public";
-
-          features = {
-            wiki.enable = false;
-            issues.enable = true;
-            projects.enable = false;
-            discussions.enable = false;
-          };
-
-          is_template = false;
-          is_archived = false;
-
-          pull = {
-            merge.enable = true;
-            squash.enable = false;
-            rebase.enable = false;
-            auto_merge = true;
-            delete_branch_on_merge = true;
-            update_branch = true;
-          };
-
-          actions = {
-            enable = true;
-            policy = "all";
-            default_token_permissions = "read";
-            allow_pr_approval = false;
-          };
-
-          rulesets = {
-            default = {
-              enforcement = "active";
-              conditions.ref_name.include = [ "~DEFAULT_BRANCH" ];
-              rules = [
-                { type = "deletion"; }
-                { type = "non_fast_forward"; }
-              ];
-            };
-          };
-        };
       }
     );
 }
